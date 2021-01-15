c95369f601e89f30617227168930b273c7dd247d
Brandon Kim <juhkim90@gmail.com>
2021-01-15 00:32:02 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Hello World") # This print statement prints out 'Hello World'
+
+# Write 5 print statements 
+# Name, Your grade, favorite color, favorite sport, favorite ice cream flavor
+
+
+print("My name is Celine Tran")
+print("I just graduated college")
+print("My favorite color is yellow")
+print("My favorite sport is Basketball")
+print("My favorite ice cream flavor is Mint Chip")
